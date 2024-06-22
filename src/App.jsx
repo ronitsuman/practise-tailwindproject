cf25ce95f3a67e269cdaa3cc7260140a60072d11
Ronit Suman <ronitsuman59@gmail.com>
2024-06-22 22:01:20 +0530

    x

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Components/Header"
+import Body from "./Components/Body"
+import Footer from "./Components/Footer"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>Hello baby </div>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Body.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>body <a href="#">link</a></div>
+  )
+}
+
+export default Body
